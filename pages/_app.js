ff76237bfd1b9a4a1b61e7bb050c8c60aaea4df6
sinasafari <m.sinasafari@gmail.com>
2022-07-31 11:55:44 +0430

    article: react native first touch v1.0.0

--- pages/_app.js
+++ pages/_app.js
@@ -17,13 +17,13 @@ import { getAnalytics } from 'firebase/analytics'
 import { useEffect } from 'react'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
 }
 
 // Initialize Firebase
@@ -33,10 +33,9 @@ const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    if(getApps().length === 0) {
+    if (getApps().length === 0) {
       // const app = initializeApp(firebaseConfig)
       // const analytics = getAnalytics(app)
-
     }
   }, [])
   return (
